01d9a2b53fc49b385e57766d955499595fcaec74
James Ng <james.ng@projapps.com>
2020-05-13 11:57:55 +0800

    Complete Stage #1

--- Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/task/hypercar/tickets/views.py
@@ -4,4 +4,4 @@ from django.http.response import HttpResponse
 
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse('')
+        return HttpResponse('<h2>Welcome to the Hypercar Service!</h2>')
